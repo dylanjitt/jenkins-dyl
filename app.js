e531ade6c4ac2cbc407b147a37506f1e3b503bdb
Dylan Jitton <dylanjitton2.0@gmail.com>
2024-12-02 09:01:00 -0400

    update

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ app.get('/', (req, res) => {
     res.send('Hello, Jenkins!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+//app.listen(PORT, () => {
+//    console.log(`Server is running on port ${PORT}`);
+//});
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
 module.exports = app; // Para pruebas
